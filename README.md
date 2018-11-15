030e3d8c7af29f5a590ee68a26d539a2eeb91ff6
Shlomi Gutman <shlomi@voicenter.com>
2018-11-15 03:28:18 +0200

    first poc of the pdf printer

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 This is a basic set-up for a SSR using vue-router.
 
+
+###
+npm install -g cross-env webpack-cli html2pdf-node
+###
+...
 For more information go to the following link:
 
+based of :https://vuejsdevelopers.com/2017/12/11/vue-ssr-router/
--- vue-ssr/server.js
+++ vue-ssr/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const server = express();
 const fs = require('fs');
 const path = require('path');
+
+const html2pdf = require("html2pdf-node");
 //obtain bundle
 const bundle =  require('./dist/server.bundle.js');
 //get renderer from vue server renderer
@@ -14,19 +16,19 @@ const renderer = require('vue-server-renderer').createRenderer({
 server.use('/dist', express.static(path.join(__dirname, './dist')));
 
 //start server
-server.get('*', (req, res) => { 
-    
-    bundle.default({ url: req.url }).then((app) => {    
+server.get('*', (req, res) => {
+
+    bundle.default({ url: req.url }).then((app) => {
         //context to use as data source
         //in the template for interpolation
         const context = {
             title: 'Vue JS - Server Render',
             meta: `
-                <meta description="vuejs server side render">
+                <meta description="1vuejs server side render"></meta>
             `
         };
 
-        renderer.renderToString(app, context, function (err, html) {   
+        renderer.renderToString(app, context, function (err, html) {
             if (err) {
               if (err.code === 404) {
                 res.status(404).end('Page not found')
@@ -34,12 +36,26 @@ server.get('*', (req, res) => {
                 res.status(500).end('Internal Server Error')
               }
             } else {
-              res.end(html)
+
+              console.log('html',html);
+              html2pdf.generatePdfOfHtml( html.toString())
+                .then(pdfFilePath=>{
+                  console.log('pdfFilePath',pdfFilePath)
+
+                  res.end(html)
+                })
+                .catch(err=>{
+                  console.log('err',err)
+                  res.end(err)
+                });
+
+
+
             }
-          });        
+          });
     }, (err) => {
         console.log(err);
-    });  
-});  
+    });
+});
 
 server.listen(8080);
